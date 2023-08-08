434c672f6ed387c1d0ea92de2863bcef947d4fe8
dwhyq <dwhyq@outlook.com>
2023-08-08 18:35:25 +0100

    feat:  create product filter based on price

--- app/Cart/page.js
+++ app/Cart/page.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const Cart = ({ cartItems }) => {
+  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+
   return (
-    <div className="border p-4">
+    <div className="border p-4 mb-3 mt-3 rounded-md">
       <h2 className="font-bold">Shopping Cart</h2>
-      {/* Display cart items and total */}
+      {cartItems.map((item) => (
+        <div key={item.id} className="flex justify-between items-center mt-2">
+          <p>{item.title}</p>
+          <p className='bg-orange-500 text-white rounded-lg px-1 font-bold text-md'>${item.price}</p>
+        </div>
+      ))}
+      <div className="mt-4 font-bold bg-green-700 rounded-md w-fit p-1 text-white">Total: ${totalPrice}</div>
     </div>
   );
 };
